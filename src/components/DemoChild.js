f19f993d86de0ad06225da34d4d999418fb92141
Jeremy Lim <jeremy.lim@fdmgroup.com>
2020-08-02 15:10:16 +0800

    added new demoparent and demochild

--- src/components/DemoChild.js
+++ src/components/DemoChild.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Dropdown from "../../node_modules/react-bootstrap/Dropdown";
 
 class DemoChild extends Component {
   // event happens here and gets raised
@@ -6,9 +7,19 @@ class DemoChild extends Component {
     // return this.props.countries.map((country) => (
     //   <button onClick={this.props.handleEvent}>{country.value}</button>
     // ));
-    return this.props.countries.map((country) => (
-      <button onClick={this.props.handleEvent}>{country.value}</button>
-    ));
+    if (this.props.countries.length === 0) {
+      return <Dropdown.Item>Empty</Dropdown.Item>;
+    } else {
+      return this.props.countries.map((country) => (
+        <Dropdown.Item
+          key={country.id}
+          eventKey={country.value}
+          onClick={this.props.handleEvent.bind(this, country.value)}
+        >
+          {country.value}
+        </Dropdown.Item>
+      ));
+    }
   }
 }
 export default DemoChild;
--- src/components/DemoParent.js
+++ src/components/DemoParent.js
@@ -1,29 +1,111 @@
 import React, { Component } from "react";
 import DemoChild from "./DemoChild";
+import Dropdown from "../../node_modules/react-bootstrap/Dropdown";
+import FormControl from "../../node_modules/react-bootstrap/FormControl";
 
 class DemoParent extends Component {
   state = {
     countries: [
       { id: 1, value: "Singapore" },
       { id: 2, value: "Malaysia" },
+      { id: 3, value: "Thailand" },
+      { id: 4, value: "Cambodia" },
+      { id: 5, value: "Hawaii" },
+      { id: 6, value: "Japan" },
+    ],
+    Vcountries: [
+      { id: 1, value: "Singapore" },
+      { id: 2, value: "Malaysia" },
+      { id: 3, value: "Thailand" },
+      { id: 4, value: "Cambodia" },
+      { id: 5, value: "Hawaii" },
+      { id: 6, value: "Japan" },
     ],
   };
 
-  // handle event and change state
-  handleEvent = () => {
+  //   handle event and change state
+  handleChange = (val, visibleCountries) => {
+    console.log(
+      this.state.countries.filter((country) =>
+        country.value.toLowerCase().includes(val)
+      )
+    );
+
     this.setState({
-      countries: this.state.countries.filter((item) => item.id !== 1),
+      Vountries: this.state.Vcountries.filter((country) =>
+        country.value.toLowerCase().includes(val)
+      ),
     });
+    console.log(this.state.Vcountries);
+  };
+
+  handleEvent = (event, eventKey) => {
+    console.log(event);
   };
 
   render() {
+    const max = this.state.countries.length;
+    const n = 3;
+    let visibleCountries = [];
+    // for (let i = 0; i < n; i++) {
+    //   visibleCountries.push(this.state.countries[i]);
+    // }
+
+    const options = [
+      { id: 1, value: "Singapore" },
+      { id: 2, value: "Malaysia" },
+      { id: 3, value: "Thailand" },
+      { id: 4, value: "Cambodia" },
+      { id: 5, value: "Hawaii" },
+      { id: 6, value: "Japan" },
+    ];
+
+    const filteredOptions = (searchQuery) =>
+      options.filter(
+        (option) =>
+          option.value.toLowerCase().includes(searchQuery.toLowerCase()) ||
+          !searchQuery
+      );
+
     return (
-      <DemoChild
-        handleEvent={this.handleEvent}
-        countries={this.state.countries}
-      />
+      <Dropdown>
+        <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
+          Select a location
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <FormControl
+            autoFocus
+            className="mx-3 my-2 w-auto"
+            placeholder="Type to filter..."
+            onChange={(e) => {
+              this.handleChange(e.target.value, visibleCountries);
+              //   setValue(e.target.value);
+              //   handleChange(e.target.value);
+            }}
+          />
+
+          <DemoChild
+            handleEvent={this.handleEvent}
+            countries={visibleCountries}
+          />
+        </Dropdown.Menu>
+      </Dropdown>
     );
   }
 }
+
+const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+  <a
+    href=""
+    ref={ref}
+    onClick={(e) => {
+      e.preventDefault();
+      onClick(e);
+    }}
+  >
+    {children}
+    &#x25bc;
+  </a>
+));
 
 export default DemoParent;
